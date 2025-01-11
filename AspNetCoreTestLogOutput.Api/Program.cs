38f6375600ef73cb49d31e691858393e937a61a1
IanWold <ilwold@hotmail.com>
2025-01-11 13:57:11 -0600

    Clean up Program

--- AspNetCoreTestLogOutput.Api/Program.cs
+++ AspNetCoreTestLogOutput.Api/Program.cs
@@ -1,14 +1,14 @@
+namespace AspNetCoreTestLogOutput.Api;
+
 public class Program
 {
     public static void Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
-        var app = builder.Build();
+        var app = WebApplication.CreateBuilder(args).Build();
 
         app.MapPut("/test", (ILogger<Program> logger) =>
-        {
-            logger.LogError("Test error!");
-        });
+            logger.LogError("Test error!")
+        );
 
         app.Run();
     }
